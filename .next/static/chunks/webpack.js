9a1c1b03df305f30ae2a0529fed9f31c4bf53b92
MohamedAly8 <mohamed_alyaly@outlook.com>
2022-01-16 04:16:05 -0500

    Added Hero section, SVG animation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45b80fc73d11f7d5c533"; }
+/******/ 		__webpack_require__.h = function() { return "db4df6bdef252597a80a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,23 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+     <SectionTitle main center>
+       Hi there, <br></br>
+       I'm Mohamed
+     </SectionTitle>
+
+     <SectionText>
+       Software Engineer with crazy ambitions and a positive attitude
+     </SectionText>
+
+     <Button>
+       My Resume
+     </Button>
+
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;

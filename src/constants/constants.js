dc396a4feda02510e024a585321d751988d9dc85
Mohamed Aly <mohamed_alyaly@outlook.com>
2025-05-19 14:52:48 -0400

    Updated job descrpition

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,89 +1,92 @@
 export const projects = [
   {
-    title: 'Ride&Tour Sharing Full Stack Solution',
-    description: "An iOS/Android Ride Sharing app that offers/request carpools, city tours, and recurring carpools. User data is encrypted and authenticated with Firebase.",
-      image: '/images/CarpoolerDemo.jpeg',
-      tags: ['React Native', 'Firebase', 'NodeJs','Restful API'],
-    source: 'https://github.com/MohamedAly8/Carpoolr',
-    visit: 'https://github.com/MohamedAly8/Carpoolr',
+    title: "Ride&Tour Sharing Full Stack Solution",
+    description:
+      "An iOS/Android Ride Sharing app that offers/request carpools, city tours, and recurring carpools. User data is encrypted and authenticated with Firebase.",
+    image: "/images/CarpoolerDemo.jpeg",
+    tags: ["React Native", "Firebase", "NodeJs", "Restful API"],
+    source: "https://github.com/MohamedAly8/Carpoolr",
+    visit: "https://github.com/MohamedAly8/Carpoolr",
     id: 0,
   },
   {
-    title: 'SincerelyAI - AI Enabled Text Transformation',
-    description:"A web app that uses fine-tuned GPT to generate a personalized professional emails based on user input. The app also uses a NodeJS backend to handle user requests and a React frontend to display the response.",
-    image: '/images/sincerely.png',
-    tags: ['NodeJS', 'React', 'GPT', 'Restful API'],
-    source: 'https://github.com/MohamedAly8/Sincerely-AI',
-    visit: 'https://sincerelyai.netlify.app',
+    title: "SincerelyAI - AI Enabled Text Transformation",
+    description:
+      "A web app that uses fine-tuned GPT to generate a personalized professional emails based on user input. The app also uses a NodeJS backend to handle user requests and a React frontend to display the response.",
+    image: "/images/sincerely.png",
+    tags: ["NodeJS", "React", "GPT", "Restful API"],
+    source: "https://github.com/MohamedAly8/Sincerely-AI",
+    visit: "https://sincerelyai.netlify.app",
     id: 1,
   },
   {
-    title: '4-Way Intersection Advanced Simulator',
-    description: "An advanced simulator for a 4-way intersection that models realistic traffic patterns. The simulator uses Python to simulate multiple vehicles moving through the intersection with various speeds, turning patterns, and preferences. It also detects/predicts accidents based on driver behavior",
-    image: '/images/intersection.png',
-    tags: ['Python', 'Machine Learning', 'Data Analysis', 'Restful API'],
-    source: 'https://github.com/MohamedAly8/4Way-Intersection-Simulation',
-    visit: 'https://github.com/MohamedAly8/4Way-Intersection-Simulation',
+    title: "4-Way Intersection Advanced Simulator",
+    description:
+      "An advanced simulator for a 4-way intersection that models realistic traffic patterns. The simulator uses Python to simulate multiple vehicles moving through the intersection with various speeds, turning patterns, and preferences. It also detects/predicts accidents based on driver behavior",
+    image: "/images/intersection.png",
+    tags: ["Python", "Machine Learning", "Data Analysis", "Restful API"],
+    source: "https://github.com/MohamedAly8/4Way-Intersection-Simulation",
+    visit: "https://github.com/MohamedAly8/4Way-Intersection-Simulation",
     id: 2,
   },
   {
-    title: 'Breathe Mental Health App',
-    description: "A breath monitoring and chatbot web app that can regulates and logs user breathing patterns. The webapp also includes a chatbot to identify user mental health issues using Machine Learning and guides the user to the helpful recoures.",
-      image: '/images/Breathe.png',
-      tags: ['React', 'Node', 'Dialogflow', 'AuthO', 'GraphCMS'],
-    source: 'https://github.com/MohamedAly8/Breathe-Hackathon-May-2021',
-    visit: 'https://github.com/MohamedAly8/Breathe-Hackathon-May-2021',
+    title: "Breathe Mental Health App",
+    description:
+      "A breath monitoring and chatbot web app that can regulates and logs user breathing patterns. The webapp also includes a chatbot to identify user mental health issues using Machine Learning and guides the user to the helpful recoures.",
+    image: "/images/Breathe.png",
+    tags: ["React", "Node", "Dialogflow", "AuthO", "GraphCMS"],
+    source: "https://github.com/MohamedAly8/Breathe-Hackathon-May-2021",
+    visit: "https://github.com/MohamedAly8/Breathe-Hackathon-May-2021",
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  { 
-    date: 'May 2024 - Present', 
-    title: 'Associate Software Engineer', 
-    subtitle: 'Veeva Systems', 
-    text: 'Working with React and Java on VaultCRM (Service Center), a customer relationship management system that helps life sciences companies manage their customer interactions. I am responsible for developing new features, fixing bugs, and maintaining the system.', 
-    icon: '/images/veeva.jpg', 
-    tagArray: ['React', 'Java', 'Spring Boot', 'Jest', 'Mockito'] 
+  {
+    date: "May 2024 - Present",
+    title: "Associate Software Engineer",
+    subtitle: "Veeva Systems",
+    text: "Working with React, Typescript, and Java on VaultCRM (Service Center), a customer relationship management system that helps life sciences companies manage their customer interactions. I am responsible for developing new features, fixing bugs, and maintaining the system.",
+    icon: "/images/veeva.jpg",
+    tagArray: ["React", "Typescript", "Java", "Spring Boot", "Jest", "Mockito"],
   },
-  { 
-    date: 'May-Sep 2023', 
-    title: 'Data Engineer Intern', 
-    subtitle: 'RBC', 
-    text: 'At RBC, I led a team of 5 in developing a PoC for a virtual assistant app integrating fine-tuned GPT. The project enhanced operational efficiency by reducing routine task time by 60% for 200+ users. Developed a scalable system architecture and a robust data pipeline handling 3TB+ data daily.', 
-    icon: '/images/rbc.png', 
-    tagArray: ['LLM', 'Angular',  'NodeJS', 'Python', 'Kafka'] 
+  {
+    date: "May-Sep 2023",
+    title: "Data Engineer Intern",
+    subtitle: "RBC",
+    text: "At RBC, I led a team of 5 in developing a PoC for a virtual assistant app integrating fine-tuned GPT. The project enhanced operational efficiency by reducing routine task time by 60% for 200+ users. Developed a scalable system architecture and a robust data pipeline handling 3TB+ data daily.",
+    icon: "/images/rbc.png",
+    tagArray: ["LLM", "Angular", "NodeJS", "Python", "Kafka"],
   },
-  { 
-    date: 'May-Sep 2022', 
-    title: 'AI Applications Engineer Intern', 
-    subtitle: 'IBM', 
-    text: 'At IBM, I automated manual SQL tasks resulting in a 300% improvement in processing time. Developed a full-stack web app empowering clients to make informed business decisions and save significantly. Led the development and presentation of several high-impact PoC demos, contributing to the acquisition of key projects.', 
-    icon: '/images/ibm.png', 
-    tagArray: ['SQL', 'Python', 'React', 'NodeJS'] 
+  {
+    date: "May-Sep 2022",
+    title: "AI Applications Engineer Intern",
+    subtitle: "IBM",
+    text: "At IBM, I automated manual SQL tasks resulting in a 300% improvement in processing time. Developed a full-stack web app empowering clients to make informed business decisions and save significantly. Led the development and presentation of several high-impact PoC demos, contributing to the acquisition of key projects.",
+    icon: "/images/ibm.png",
+    tagArray: ["SQL", "Python", "React", "NodeJS"],
   },
-  { 
-    date: 'Sep-Dec 2021', 
-    title: 'Software Engineer Intern', 
-    subtitle: 'Thales Group', 
-    text: 'At Thales Group, I improved autonomous vehicle data processing by 65%, saving significant costs. Utilized Agile methodologies to deliver a complex software solution, and developed object detection models enhancing obstacle recognition accuracy by 25%, contributing to system safety and efficiency.', 
-    icon: '/images/thales.png', 
-    tagArray: ['Python', 'React', 'Docker', 'MongoDB', 'TensorFlow', 'Agile'] 
+  {
+    date: "Sep-Dec 2021",
+    title: "Software Engineer Intern",
+    subtitle: "Thales Group",
+    text: "At Thales Group, I improved autonomous vehicle data processing by 65%, saving significant costs. Utilized Agile methodologies to deliver a complex software solution, and developed object detection models enhancing obstacle recognition accuracy by 25%, contributing to system safety and efficiency.",
+    icon: "/images/thales.png",
+    tagArray: ["Python", "React", "Docker", "MongoDB", "TensorFlow", "Agile"],
   },
-  { 
-    date: 'Jan-Sep 2021', 
-    title: 'Full Stack Software Engineer Intern', 
-    subtitle: 'TELUS', 
-    text: 'At TELUS, I led the development of an internal web app increasing operational efficiency by 40%. Built PoC Machine Learning models that sparked a rise in stakeholder investment and automated data stream processing, replacing hours of manual work.', 
-    icon: '/images/telus.png', 
-    tagArray: ['React', 'Python', 'TensorFlow', 'Pandas'] 
+  {
+    date: "Jan-Sep 2021",
+    title: "Full Stack Software Engineer Intern",
+    subtitle: "TELUS",
+    text: "At TELUS, I led the development of an internal web app increasing operational efficiency by 40%. Built PoC Machine Learning models that sparked a rise in stakeholder investment and automated data stream processing, replacing hours of manual work.",
+    icon: "/images/telus.png",
+    tagArray: ["React", "Python", "TensorFlow", "Pandas"],
   },
 ];
-
 
 export const Memoji = [
   {
     id: 1,
-    image: '/images/Memoji.png',
-  }
-]
+    image: "/images/Memoji.png",
+  },
+];
